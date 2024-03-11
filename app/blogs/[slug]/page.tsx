c4d3610de3ad3ef054dbfc86467de1062d52bec9
sidchopp <sidchopp@gmail.com>
2024-03-11 17:33:40 -0600

    GraphQL API used for contentful

---
+++ app/blogs/[slug]/page.tsx
@@ -0,0 +1,41 @@
+import { getAllBlogs, getBlog } from "@/lib/api";
+import { notFound } from "next/navigation";
+
+export async function generateStaticParams() {
+  const allArticles = await getAllBlogs();
+
+  const validArticles = allArticles.filter(
+    (blog) => blog.slug && typeof blog.slug === "string"
+  );
+
+  return validArticles.map((blog) => ({
+    params: {
+      slug: blog.slug,
+    },
+  }));
+}
+
+export default async function BlogPage({ params }) {
+  const blog = await getBlog(params?.slug);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <section className="w-full">
+        <div className="container space-y-12 px-4 md:px-6">
+          <div className="space-y-4">
+            <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl">
+              {blog?.title}
+            </h1>
+            <p className="max-w-[900px] text-zinc-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-zinc-400">
+              {blog?.description}
+            </p>
+          </div>
+          <div className="space-y-8 lg:space-y-10">
+            <div className="space-y-4 md:space-y-6">
+              <div className="space-y-2"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { createClient } from "contentful";
-import { FormattedDate } from "../components/FormattedDate";
+import { getAllBlogs } from "@/lib/api";
+import { FormattedDate } from "@/components/FormattedDate";
+import Link from "next/link";
 
 interface Metadata {
   tags: string[];
@@ -29,47 +31,62 @@ interface DataType {
   fields: Fields;
 }
 
-async function getData() {
-  const client = createClient({
-    space: process.env.CONTENTFULL_SPACE_ID as string,
-    accessToken: process.env.CONTENTFULL_ACCESS_TOKEN as string,
-  });
-
-  const res = await client.getEntries({
-    content_type: "blogPost",
-  });
-
-  return res.items;
-}
-
 export default async function Home() {
-  const data: DataType[] = await getData();
-
-  console.log(data);
+  const blogs = await getAllBlogs();
 
   return (
-    <main className="p-20">
-      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center py-10">
-        Sham Mohan{" "}
-        <span className="text-blue-600 dark:text-blue-500">Chopra</span>
-      </h1>
-      <div className="container mx-auto px-4 py-8">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-9">
-          {data.map((post) => (
-            <div key={post.sys.id}>
-              <h2 className="mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl dark:text-white">
-                {post.fields.title}
-              </h2>
-              <p className="text-sm text-gray-400 py-2">
-                <FormattedDate date={post.fields.date} />
-              </p>
-              <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-200 text-left">
-                {post.fields.description}
-              </p>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <section className="w-full pt-12">
+        <div className="mx-auto container space-y-12 px-4 md:px-6">
+          <div className="flex flex-col items-center justify-center space-y-4 text-center">
+            <div className="space-y-2">
+              <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center py-10">
+                Sham Mohan{" "}
+                <span className="text-blue-600 dark:text-blue-500">Chopra</span>
+              </h1>
             </div>
-          ))}
+          </div>
+          <div className="space-y-12">
+            <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+              {blogs.map((blog) => (
+                <article
+                  key={blog.sys.id}
+                  className="h-full flex flex-col rounded-lg shadow-lg overflow-hidden"
+                >
+                  {/* <Image
+                    alt="placeholder"
+                    className="aspect-[4/3] object-cover w-full"
+                    height="263"
+                    src={blog.blogImage.url}
+                    width="350"
+                  /> */}
+                  <div className="flex-1 p-6">
+                    <Link href={`/blogs/${blog?.slug}`}>
+                      <h3 className="text-2xl font-bold leading-tight text-zinc-900 dark:text-zinc-50  py-4">
+                        {blog.title}
+                      </h3>
+                    </Link>
+                    <p className="text-sm text-gray-400 pb-2">
+                      <FormattedDate date={blog.date} />
+                    </p>
+                    <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-200 text-left">
+                      {blog.description}
+                    </p>
+                    <div className="flex justify-end">
+                      <Link
+                        className="inline-flex h-10 items-center justify-center text-sm font-medium"
+                        href={`/blogs/${blog?.slug}`}
+                      >
+                        Read More →
+                      </Link>
+                    </div>
+                  </div>
+                </article>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
+      </section>
     </main>
   );
 }
