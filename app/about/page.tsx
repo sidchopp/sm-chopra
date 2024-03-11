3dbd66c0207e87abbc5dfa68c7e796754967a54a
sidchopp <sidchopp@gmail.com>
2024-03-11 12:28:48 -0600

    init

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt in rerum
+        ipsum atque, qui provident vel et nam similique aut rem! Animi, deleniti
+        in. Iste, quis. Cumque, explicabo? Laborum, id!
+      </p>
+    </div>
+  );
+};
+
+export default About;
