de89a029dc2ed3eeff51e543e149f269e8fcc94b
sidchopp <sidchopp@gmail.com>
2024-03-11 20:19:06 -0600

    Image used, subTiles in main page and description in indivudual blog

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { createClient } from "contentful";
 import { getAllBlogs } from "@/lib/api";
 import { FormattedDate } from "@/components/FormattedDate";
 import Link from "next/link";
+import Image from "next/image";
 
 interface Metadata {
   tags: string[];
@@ -53,26 +54,26 @@ export default async function Home() {
                   key={blog.sys.id}
                   className="h-full flex flex-col rounded-lg shadow-lg overflow-hidden"
                 >
-                  {/* <Image
+                  <Image
                     alt="placeholder"
                     className="aspect-[4/3] object-cover w-full"
                     height="263"
-                    src={blog.blogImage.url}
+                    src={blog?.image?.url}
                     width="350"
-                  /> */}
+                  />
                   <div className="flex-1 p-6">
                     <Link href={`/blogs/${blog?.slug}`}>
                       <h3 className="text-2xl font-bold leading-tight text-zinc-900 dark:text-zinc-50  py-4">
-                        {blog.title}
+                        {blog?.title}
                       </h3>
                     </Link>
                     <p className="text-sm text-gray-400 pb-2">
-                      <FormattedDate date={blog.date} />
+                      <FormattedDate date={blog?.date} />
                     </p>
                     <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-200 text-left">
-                      {blog.description}
+                      {blog?.subTitle}
                     </p>
-                    <div className="flex justify-end">
+                    <div className="flex justify-start">
                       <Link
                         className="inline-flex h-10 items-center justify-center text-sm font-medium"
                         href={`/blogs/${blog?.slug}`}
