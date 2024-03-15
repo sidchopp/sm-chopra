efce66aaeaabb8432c042d5f8340d96adfbc5bcd
sidchopp <siddchopp@gmail.com>
2024-03-15 16:55:33 -0600

    revalidate tags blogs added

--- app/api/revalidation/route.js
+++ app/api/revalidation/route.js
@@ -1,4 +1,6 @@
+import { NextApiRequest } from "next";
 import { NextResponse } from "next/server";
+import { revalidateTag } from "next/cache";
 
 export async function POST(request) {
   const requestHeaders = new Headers(request.headers);
@@ -7,6 +9,8 @@ export async function POST(request) {
   if (secret !== process.env.MY_SECRET_TOKEN) {
     return NextResponse.json({ message: "Invalid secret" }, { status: 401 });
   }
+
+  revalidateTag("blogs");
 
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
