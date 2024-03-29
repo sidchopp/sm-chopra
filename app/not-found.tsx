f0518d38eeeaf3f251ef44d86d25675d65c80690
sidchopp <siddchopp@gmail.com>
2024-03-29 12:07:52 -0600

    notfound page replaced error page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,11 +2,11 @@
 
 import Link from "next/link";
 
-export default function Error() {
+export default function NotFound() {
   return (
-    <div className="grid h-screen px-4  place-content-center">
+    <div className="grid h-screen px-4 place-content-center">
       <div className="text-center">
-        <h1 className="font-black text-blue-600 dark:blue-500 text-9xl">404</h1>
+        <h1 className="font-black text-blue-500 text-9xl">404</h1>
 
         <p className="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-500 sm:text-4xl">
           Page not found
@@ -15,7 +15,7 @@ export default function Error() {
         <p className="mt-4">Sorry, we could not find that page.</p>
         <Link
           href="/"
-          className="inline-block px-5 py-3 my-6 text-sm font-medium bg-blue-600 dark:bg-blue-500 rounded hover:bg-indigo-700 focus:outline-none focus:ring cursor-pointer"
+          className="inline-block px-5 py-3 my-6 text-sm font-medium bg-blue-500 rounded hover:bg-indigo-600 focus:outline-none focus:ring cursor-pointer"
         >
           Back to Home
         </Link>
